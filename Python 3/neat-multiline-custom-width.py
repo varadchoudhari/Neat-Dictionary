fe41333b302a75df9b1fcd177b7cd152fab2a898
varadchoudhari <varad.choudhari@gmail.com>
2017-11-29 21:23:15 -0500

    python3 support

---
+++ Python 3/neat-multiline-custom-width.py
@@ -0,0 +1,82 @@
+'''
+multiline_printer
+params dictionary: Dictionary that you want to print
+titles: Titles you want to give for the table
+width: List of width between the titles
+
+example:
+d = {"egg":[["Qty. 1",1], ["Qty. 2",2], ["Qty. 4",3]],
+     "bread":[["Qty. 2",2], ["Qty. 3",3]]}
+
+multiline_printer(d,["Item","Quantity","Price"],[6,10,10])
+
+OUTPUT:
+Item  Quantity  Price
+egg   Qty. 1    1
+egg   Qty. 2    2
+egg   Qty. 4    3
+bread Qty. 2    2
+bread Qty. 3    3
+'''
+
+def multiline_printer(dictionary,titles,width):
+    formatting = ""
+    for number in width:
+        formatting += "{:<" + str(number) + "}"
+    print formatting.format(*titles)
+    for key, value in dictionary.iteritems():
+        if isinstance(value[0],list):
+            for sublist in value:
+                sv = []
+                for subvalue in sublist:
+                    sv.append(subvalue)
+                print formatting.format(key,*sv)
+        else:
+            sv = []
+            for subvalue in value:
+                sv.append(subvalue)
+            print formatting.format(key, *sv)
+'''
+multiline_writer
+params dictionary: Dictionary that you want to print
+titles: Titles you want to give for the table
+width: List of width between the titles
+file: File you want to write dictionary to
+
+example:
+d = {"egg":[["Qty. 1",1], ["Qty. 2",2], ["Qty. 4",3]],
+     "bread":[["Qty. 2",2], ["Qty. 3",3]]}
+
+file = open("multiline_writer.txt", "w")
+
+multiline_writer(d,["Item","Quantity","Price"],[6,10,10],file)
+
+OUTPUT FILE:
+Item  Quantity  Price
+egg   Qty. 1    1
+egg   Qty. 2    2
+egg   Qty. 4    3
+bread Qty. 2    2
+bread Qty. 3    3
+'''
+
+def multiline_writer(dictionary,titles,width, file):
+    formatting = ""
+    for number in width:
+        formatting += "{:<" + str(number) + "}"
+    file.write(formatting.format(*titles))
+    file.write("\n")
+    for key, value in dictionary.iteritems():
+        if isinstance(value[0],list):
+            for sublist in value:
+                sv = []
+                for subvalue in sublist:
+                    sv.append(subvalue)
+                file.write(formatting.format(key,*sv))
+                file.write("\n")
+        else:
+            sv = []
+            for subvalue in value:
+                sv.append(subvalue)
+            file.write(formatting.format(key, *sv))
+            file.write("\n")
